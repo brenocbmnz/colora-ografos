dd544b3820d20c81ccfd480cb5f250fb0dc9960b
brenocbmnz <brenocbmnz@gmail.com>
2024-12-05 11:52:20 -0300

    Mudando o printf

--- corgrafo.cpp
+++ corgrafo.cpp
@@ -5,7 +5,7 @@
 
 void coloracaoGulosa(int grafo[MAX_VERTICES][MAX_VERTICES], int numVertices) {
     int resultado[MAX_VERTICES];
-    resultado[0] = 0; // Primeiro vértice colorido com 0
+    resultado[0] = 0; // Pinta o primeiro vértice com 0
 
   
     for (int i = 1; i < numVertices; i++) {
@@ -14,21 +14,21 @@ void coloracaoGulosa(int grafo[MAX_VERTICES][MAX_VERTICES], int numVertices) {
     
     bool disponivel[MAX_VERTICES];
     
-    // Colorir
+    // Colore
     for (int u = 1; u < numVertices; u++) {
-        // Reiniciar
+        // Reinicia
         for (int i = 0; i < numVertices; i++) {
             disponivel[i] = true;
         }
         
-        // Marcar vértices adjacentes como indisponíveis
+        // Marca os vértices adjacentes como indisponíveis
         for (int v = 0; v < numVertices; v++) {
             if (grafo[u][v] == 1 && resultado[v] != -1) {
                 disponivel[resultado[v]] = false;
             }
         }
         
-        // Encontrar a menor cor disponível
+        // Encontra a menor cor disponível
         for (int cr = 0; cr < numVertices; cr++) {
             if (disponivel[cr]) {
                 resultado[u] = cr;
@@ -38,11 +38,12 @@ void coloracaoGulosa(int grafo[MAX_VERTICES][MAX_VERTICES], int numVertices) {
     }
     
 
-    printf("\nResultado da Coloração do Grafo:\n");
-    printf("Vértice\tCor\n");
-    for (int i = 0; i < numVertices; i++) {
-        printf("%d\t%d\n", i, resultado[i]);
-    }
+	printf("\nColoracao do Grafo:\n");
+	printf("%-10s %-10s\n", "Vertice", "Cor");
+	printf("-------------------\n");
+		for (int i = 0; i < numVertices; i++) {
+	    	printf("%-10d %-10d\n", i, resultado[i]);
+	}
 }
 
 int main() {
